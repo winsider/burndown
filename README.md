58576d221c59dbedf91faf65be31bfa9b8b50779
winsider <lars@tungen.net>
2019-10-12 11:00:28 +0200

    Project init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Burnddown
+Zero cost abstration web tool.
+
+## Getting started
+cd build
+cmake ..
+make
---
+++ apps/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Burndown\n";
+    std::cout << "- zero cost abstraction web\n";
+}
