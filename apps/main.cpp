8ef37bfb50c8939ca4a0f9d9eca05702749466ca
winsider <lars@tungen.net>
2019-11-06 00:09:40 +0100

    Removed punchline

--- apps/main.cpp
+++ apps/main.cpp
@@ -7,14 +7,13 @@ int main(int argc, char *argv[])
 {
     std::ios_base::sync_with_stdio(false);
     std::cout << "Burndown\n";
-    std::cout << "- zero cost abstraction web\n";
     try
     {
         std::string file_name("./../../docs/example.csp"); 
         auto tmpl = Template::from_file(file_name);
         tmpl->write_translation_unit();
     }
-    catch (std::runtime_error& err)
+    catch (const std::runtime_error& err)
     {
         std::cerr << "EXCEPTION: " << err.what() << '\n';
     }
